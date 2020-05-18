fda38e91f9d3085b08ab5ac0516298f99275a31d
Adnan Joraid <joraid@shernet.sheridancollege.ca>
2020-05-18 15:31:18 -0400

    added class data, fixed an issue where value doesn't exist in json file

--- main.py
+++ main.py
@@ -3,7 +3,36 @@ import requests
 API_KEY = None
 PROJECT_TOKEN = None
 RUN_TOKEN = None
+class Data():
+    '''
+    responsible for getting data regarding COVID-19 such as:
+    1) number of cases
+    2) number of deaths 
+    3) number of recovery 
+    4) number of population for each country
+    '''
+    def __init__(self, apiKey, projectToken):
+        self.apiKey = apiKey
+        self.params = {'api_key' : self.apiKey}
+        self.projectToken = projectToken
+        self.data = self.getData()
+        
+        
+    
+    def getData(self): #gets all the data that was scrapped from the website. 
+        response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.projectToken}/last_ready_run/data',params=self.params)
+        data = json.loads(response.text)
+        return data
 
-response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data',params={"api_key":API_KEY})
-data = json.loads(response.text)
-print(data)
+    def getTotalCases(self):
+        data = self.data['total']
+
+        for content in data:
+            if content['name'] == "Coronavirus Cases:":
+                return content['values']
+
+
+data = Data(API_KEY, PROJECT_TOKEN)
+allData = data.getData()
+totalCases = data.getTotalCases()
+print(totalCases)
